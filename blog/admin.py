bcca01fe656c0a90f25debc8f2bc4bf7fa8b082f
Aidamir Lovpache <aidamatrix@gmail.com>
2018-12-12 09:34:28 +0300

    Post admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from blog.models import Post
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    raw_id_fields = ['user']
+    fields = ['user', 'title', 'content']
+    list_display = ['user', 'title', 'created']
+    list_filter = ['user', 'created']
+
+
+admin.site.register(Post, PostAdmin)
